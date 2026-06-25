636582296b1c2a37be5ebdc41e3a4af969a8dac3
Israel539 <if739871@gmail.com>
2026-06-24 21:48:41 -0300

    refazendo

--- fasmicro/core/Helper/functionsHelper.php
+++
@@ -1,7 +0,0 @@
-<?php
-/** 
- * Função para destruir variáveis já usadas
-*/
-function destroy(&$var){
-    unset($var);
-}
--- fasmicro/public/assests/bootstrap/js/teste.js
+++
@@ -1,8 +0,0 @@
-function teste(){
-    const myModal = document.getElementById('exampleModal');
-    myModal.addEventListener('shown.bs.modal', () => {
-        myInput.focus();
-    })
-}
-
-// ===============================================================
